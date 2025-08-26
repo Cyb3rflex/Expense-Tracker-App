faa3e5e0e8ed546b289c44be54b0291b727f1e09
hajjisco <hajjisco12@gmail.com>
2025-08-26 22:45:28 +0100

    progress

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+// const { createElement } = require("react")
+const text = document.querySelector(".text")
+const amount = document.querySelector("#amount")
 const showExpence = document.querySelector('.list')
 const addTransaction =document.querySelector('.btn')
 addTransaction.addEventListener('click', function(){
-    prompt('doing great')
+    // prompt('doing great')    
+    const li = document.createElement('li')
+    li.className = 'createdElement'
+    li.textContent= `new ${text.value} ${amount.value} `
+    showExpence.appendChild(li)
     
 })
