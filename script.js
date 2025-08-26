0f267ac3d2a21718ea22bff312188a75b27a7009
hajjisco <hajjisco12@gmail.com>
2025-08-26 13:56:35 +0100

    progress

---
+++ script.js
@@ -0,0 +1,6 @@
+const showExpence = document.querySelector('.list')
+const addTransaction =document.querySelector('.btn')
+addTransaction.addEventListener('click', function(){
+    prompt('doing great')
+    
+})
